6a5f3cfcd684b44b21ad3fe9b3667f74c70c89b7
Amimul Ehasan Shakib <maeshakib@Amimuls-Mac.local>
2016-06-29 02:55:08 -0700

    first

---
+++ TubayinMaeshakib/PortfoliCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  PortfoliCollectionViewCell.swift
+//  TubayinMaeshakib
+//
+//  Created by Amimul Ehasan Shakib on 6/28/16.
+//  Copyright © 2016 Amimul Ehasan Shakib. All rights reserved.
+//
+
+import UIKit
+
+class PortfoliCollectionViewCell: UICollectionViewCell {
+    
+}
---
+++ TubayinMaeshakib/ServicesTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ServicesTableViewCell.swift
+//  TubayinMaeshakib
+//
+//  Created by Amimul Ehasan Shakib on 6/13/16.
+//  Copyright © 2016 Amimul Ehasan Shakib. All rights reserved.
+//
+
+import UIKit
+
+class ServicesTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
